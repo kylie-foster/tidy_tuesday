33767ffef0f6b6312b8b47688131c92df6332f4c
kylie-foster <kyliefos@gmail.com>
2019-05-10 22:39:11 +0800

    Finishing Week19

---
+++ Week19_2019/code_plot.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(ggalt) # for ggalt
+library(ggthemes)
+
+student_ratio <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-07/student_teacher_ratio.csv") %>%
+  mutate_if(is.character, as.factor) %>%# converting characters to factors
+  filter(grepl('income', country)) %>% # selecting only those observations that include "income"
+  filter(year == c(min(year), max(year))) %>% # selecting the highest and lowest years to use as endpoints of dumbbell plot
+  rename(value = student_ratio) %>% # renaming student_ratio variable to prevent confusion with dataframe name
+  select(indicator, country, year, value) %>%
+  spread(key = year, value = value) %>%
+  rename(year_2012 = "2012", year_2017 = "2017") %>%
+  mutate(country = str_remove(country, "income countries"))
+
+student_ratio$indicator <- factor(student_ratio$indicator, 
+                                  levels = c("Pre-Primary Education", "Primary Education",
+                                             "Lower Secondary Education", "Secondary Education",
+                                             "Upper Secondary Education")) # reordering 
+
+# choosing colours:
+col_line <- "gray"
+col_2012 <- "#a3c4dc" #light blue, 2012 
+col_2017 <- "#0e668b" # dark blue, 2017
+
+student_ratio_long <- gather(student_ratio, year_2012, year_2017, key = year, value = value) %>%
+  mutate_if(is.character, as.factor)
+
+ggplot() +
+  geom_dumbbell(data = student_ratio, 
+                aes(y = fct_rev(fct_reorder(country, year_2012)), x = year_2012, xend = year_2017),
+                colour = col_line, colour_x = col_2012, colour_xend = col_2017, size = 1.5) +
+  geom_point(data = student_ratio_long, aes(x = value, y = country, colour = year), size = 3.5) + 
+  facet_wrap( ~ indicator, ncol = 2) +
+  labs(title = "Change in student to teacher ratios from 2012 to 2017",
+       x = "Student to Teacher Ratio (lower = fewer students per teacher)",
+       y = "Country Income Level",
+       caption = "While most ratios did not change substantially between 2012 and 2017, some ratios increased.
+       \nSource: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-05-07") +
+  theme_bw(base_size = 15) +
+  theme(legend.position = c(0.75, 0.15)) +
+  scale_color_manual(values = c("#a3c4dc", "#0e668b"), label = c("2012", "2017"))
+
+ggsave("Week19_2019/student_teacher_ratios.png", height = 10, width = 8)
+
+
+
+student_ratio_2012 <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-07/student_teacher_ratio.csv") %>%
+  mutate_if(is.character, as.factor) %>%# converting characters to factors
+  filter(grepl('income', country)) %>% # selecting only those observations that include "income"
+  filter(year == 2012) 
+
+student_ratio_2017 <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-07/student_teacher_ratio.csv") %>%
+  mutate_if(is.character, as.factor) %>%# converting characters to factors
+  filter(grepl('income', country)) %>% # selecting only those observations that include "income"
+  filter(year == 2017) 
+
+%>%
+  filter(indicator == "Primary Education")
