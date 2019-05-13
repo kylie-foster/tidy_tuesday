ab12b9cb29cb143ac02e6cedf9f9d40679a94f48
kylie-foster <kyliefos@gmail.com>
2019-05-13 18:53:02 +0800

    tidying code for Week19

--- Week19_2019/code_plot.R
+++ Week19_2019/code_plot.R
@@ -17,25 +17,28 @@ student_ratio$indicator <- factor(student_ratio$indicator,
                                              "Lower Secondary Education", "Secondary Education",
                                              "Upper Secondary Education")) # reordering 
 
+student_ratio_long <- gather(student_ratio, year_2012, year_2017, key = year, value = value) %>%
+  mutate_if(is.character, as.factor) # converting dataframe back to long form (required for legend)
+
+
 # choosing colours:
-col_line <- "gray"
+col_line <- "gray" #line colour
 col_2012 <- "#a3c4dc" #light blue, 2012 
 col_2017 <- "#0e668b" # dark blue, 2017
 
-student_ratio_long <- gather(student_ratio, year_2012, year_2017, key = year, value = value) %>%
-  mutate_if(is.character, as.factor)
-
+# Plotting:
 ggplot() +
   geom_dumbbell(data = student_ratio, 
                 aes(y = fct_rev(fct_reorder(country, year_2012)), x = year_2012, xend = year_2017),
                 colour = col_line, colour_x = col_2012, colour_xend = col_2017, size = 1.5) +
-  geom_point(data = student_ratio_long, aes(x = value, y = country, colour = year), size = 3.5) + 
+  geom_point(data = student_ratio_long, aes(x = value, y = country, colour = year), size = 3.5) + # required because 
+  # geom_dumbell does not provide a legend option
   facet_wrap( ~ indicator, ncol = 2) +
   labs(title = "Change in student to teacher ratios from 2012 to 2017",
+       subtitle = "Some ratios increased between 2012 and 2017",
        x = "Student to Teacher Ratio (lower = fewer students per teacher)",
        y = "Country Income Level",
-       caption = "While most ratios did not change substantially between 2012 and 2017, some ratios increased.
-       \nSource: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-05-07") +
+       caption = "Source: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-05-07") +
   theme_bw(base_size = 15) +
   theme(legend.position = c(0.75, 0.15)) +
   scale_color_manual(values = c("#a3c4dc", "#0e668b"), label = c("2012", "2017"))
