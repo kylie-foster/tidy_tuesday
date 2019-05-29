458cc311ebba1193111ee5d5a8de4371888f64c0
kylie-foster <kyliefos@gmail.com>
2019-05-29 12:49:42 +0800

    ggridges

--- Week22_2019/wine_ratings_ggridges_unused.R
+++ Week22_2019/wine_ratings_ggridges_unused.R
@@ -5,22 +5,6 @@ wine_ratings <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidy
   mutate_if(is.character, as.factor) %>%
   mutate(value = points/price) 
 
-# group_taster <- select(wine_ratings, taster_name, value)%>%
-#   group_by(taster_name) %>%
-#   na.omit()%>%
-#   summarise(med_value = median(value))
-# 
-# ggplot(data = group_taster, aes(x = taster_name, y = med_value)) +
-#   geom_col() +
-#   coord_flip()
-# 
-# ggplot(data = wine_ratings, aes(x = value, y = taster_name)) + 
-#   geom_density_ridges(jittered_points = TRUE)
-
-# wine_ratings <- group_by(wine_ratings, taster_name) %>%
-#   filter(count(value) > 100) %>%
-#   ungroup()
-
 wine_ratings_group <- group_by(wine_ratings, taster_name)
 delay <- summarise(wine_ratings_group,
                    count = n(),
@@ -38,4 +22,3 @@ ggplot(data = filter(wine_ratings, taster_name %in% name_order), aes(x = value, 
   theme_ridges() +
   xlim(0, 15)
 
-# color by number of reviews. Maybe more experienced raters have different distributions
